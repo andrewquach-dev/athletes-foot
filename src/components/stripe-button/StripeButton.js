2df70e9ded2d60d928a72e37f3e5b5ca34ca5855
Andrew Quach <andrewrquach@gmail.com>
2022-10-28 14:56:30 -0700

    stripe integration

---
+++ src/components/stripe-button/StripeButton.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51Ly0UiCuzKM0m6wo4Pcta1z98tCNTRYd6KzbB0RYCp5zeg8EUg4iEBQ6Uh7hddqMmkqCx5le6sl7o3kbmsgHMaTe00tdCDk6aY';
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Succesful!');
+    };
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='makes Scents'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is €${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+};
+
+export default StripeCheckoutButton;
